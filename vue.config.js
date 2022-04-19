fc3d017f84ed69f9fd8b9c9f5de9752eb1332af6
beatriz lira <blmartins017@gmail.com>
2022-04-18 21:53:59 -0300

    Criação de arquivos das variáveis de ambientes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  css: {
+    loaderOptions: {
+      less:{
+          additionalData: `@import "~@/assets/css/variables.less";`
+      }
+    }
+  }
+}
