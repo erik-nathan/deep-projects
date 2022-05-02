0d0ad4a31eade811280c8701c6a8f7d7bdc0bf68
beatriz lira <blmartins017@gmail.com>
2022-05-02 19:48:58 -0300

    Adicionando produtos no arquivo db.json

--- deep_project/src/views/HomeView.vue
+++ deep_project/src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home">
-  <category-menu />
+  <CategoryMenu />
   </div>
 </template>
 
 <script>
 import CategoryMenu from '../components/CategoryMenu.vue'
-
+import axios from 'axios';
 
 export default {
   name: 'HomeView',
   components: {
     CategoryMenu
+  },
+  created() {
+    axios.get('http://localhost:3000/products').then( response => {
+      console.log(response);
+    })
   }
 }
 </script>
