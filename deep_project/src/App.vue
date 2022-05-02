713fa4f76b346a6534ddf97ff352cc51e8102685
beatriz lira <blmartins017@gmail.com>
2022-05-02 19:19:19 -0300

    Adicionando rotas no projeto

--- deep_project/src/App.vue
+++ deep_project/src/App.vue
@@ -1,22 +1,9 @@
 <template>
   <div id="app">
-     <CategoryMenu  />
-     
+    <router-view/>
   </div>
 </template>
 
-<script>
-import CategoryMenu from "./components/CategoryMenu.vue";
+<style lang="less">
 
-export default {
-  name: "App",
-  components: {
-    CategoryMenu,
-  },
-};
-</script>
-
-<style>
-#app {
-}
 </style>
---
+++ deep_project/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+  <category-menu />
+  </div>
+</template>
+
+<script>
+import CategoryMenu from '../components/CategoryMenu.vue'
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    CategoryMenu
+  }
+}
+</script>
