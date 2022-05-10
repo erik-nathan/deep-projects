5f20bc8778af2f066d57b67a7563b58dd16c5828
Júlio Barros <juliocbarros339@gmail.com>
2022-05-09 21:12:44 -0300

    Atualizando Package.js para troca de categorias

--- deep_project/src/main.js
+++ deep_project/src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import './assets/css/global.less'
 import router from './router'
+import {store} from './store/store'
+import 'es6-promise/auto'
+
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
