76b09f532fe63df16746d282d6cfbd85d57e5f8c
Abn92 <marcosfvs92@gmail.com>
2022-05-21 15:59:01 -0300

    Colocando o total no carrinho

--- deep_project/src/components/cart.vue
+++ deep_project/src/components/cart.vue
@@ -3,18 +3,31 @@
       <router-link to="/" class="cart--go-back">- Voltar</router-link>
       <h2 class="cart--title"> Seu Pedido </h2>
       <CartItem v-for="item in cartList" :key="item.id" :item="item"/>
+      <div class="cart--total">
+          <span>Total: </span>
+          <span class="price">{{getCartTotal | currency}}</span>
+      </div>
   </div>
 </template>
 
 <script>
 import CartItem from './cartItem';
+import { mapGetters } from 'vuex';
 
 export default {
  name: 'Cart',
  components: {
      CartItem
  },
+ filters: {
+        currency(value){
+            return `R$ ${value.toLocaleString('pt-br', { minimumFractionDigits : 2})}`
+        }
+    },
  computed: {
+     ...mapGetters([
+         'getCartTotal'
+     ]),
      cartList(){
          return this.$store.state.cartList;
      }
@@ -36,6 +49,18 @@ export default {
              color: @write;
          }
 
+        &--total {
+            font-weight: 600;
+            font-size: 18px;
+            text-align: right;
+            margin-top: 30px;
+            
+            .price{
+               color: @blue; 
+               padding-left: 10px;
+            }
+         }
+
           &--go-back {
              font-weight: 600;
              font-size: 18px;
@@ -44,6 +69,7 @@ export default {
          }
 
          @media @tablets {
+            //  background: @bg-color;
              width: 100%;
              min-width: unset;
              padding: 50px 20px 20px;
