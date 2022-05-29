4efd588dd56a8a9db8e1a27640eacce2c29b83bb
beatriz lira <blmartins017@gmail.com>
2022-05-29 13:34:46 -0300

    Organizando o payment view

--- deep_project/src/views/Payment.vue
+++ deep_project/src/views/Payment.vue
@@ -1,15 +1,38 @@
 <template>
-  <div>
-      pagamento
+  <div class="payment">
+      <div class="payment--content">
+            <router-link to="/" class="payment--go-back">⬅ Voltar</router-link>
+      </div>      
+      <Cart v-if="isDesktop()" />
   </div>
 </template>
 
 <script>
+import Mixin from '@/mixins/mixins'
+import Cart from '../components/cart.vue'
 export default {
-
+  components: {
+    Cart
+  },
+   mixins: [Mixin]
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+ .payment {
+   display: flex;
+
+   &--content {
+     flex-grow: 1;
+     padding: 50px;
+   
+    .payment--go-back {
+    font-weight: 600;
+    font-size: 18px;
+    text-decoration: none;
+    color: @write;
+    }
+  }
+ }
 
 </style>
