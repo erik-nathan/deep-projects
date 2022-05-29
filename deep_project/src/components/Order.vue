61649177f75a3af687665312cab74ec52e8a17fe
beatriz lira <blmartins017@gmail.com>
2022-05-29 17:46:27 -0300

    Endereço Modal

--- deep_project/src/components/Order.vue
+++ deep_project/src/components/Order.vue
@@ -42,15 +42,71 @@
                 </div>
             </div>
         </div>
-         <a>Adcionar endereço</a>
+         <a @click="onShowAdressModal">Adicionar endereço</a>
         </div>
     </form>
      <button class="primary-button" @click="orderItens">Concluir pedido</button>
+
+     <Modal :show="showAdressModal" @on-modal-close="hideAdressModal">
+        <div class="modal-content">
+            <h1>Adicionar endereço</h1>
+            <div class="input-field">
+            <label for="">{{formData.cep.label}}</label>
+            <input type="text"
+            :placeholder="formData.cep.placeholder" 
+            v-model="formData.cep.value"
+            @blur="formData.cep.isValid()" 
+            :class="{'error' : !formData.valid}">
+            <p class="error-message" v-if="!formData.cep.valid">{{formData.cep.errorMessage}}</p>
+         </div>
+
+         <div class="input-field">
+            <label for="">{{formData.city.label}}</label>
+            <input type="text"
+            :placeholder="formData.city.placeholder" 
+            v-model="formData.city.value"
+            @blur="formData.city.isValid()" 
+            :class="{'error' : !formData.valid}">
+            <p class="error-message" v-if="!formData.city.valid">{{formData.city.errorMessage}}</p>
+         </div>
+
+         <div class="adress-container">
+             <div class="input-field">
+            <label for="">{{formData.street.label}}</label>
+            <input type="text"
+            :placeholder="formData.street.placeholder" 
+            v-model="formData.street.value"
+            @blur="formData.street.isValid()" 
+            :class="{'error' : !formData.valid}">
+            <p class="error-message" v-if="!formData.street.valid">{{formData.street.errorMessage}}</p>
+            </div>
+
+            <div class="input-field">
+            <label for="">{{formData.number.label}}</label>
+            <input type="text"
+            :placeholder="formData.number.placeholder" 
+            v-model="formData.number.value"
+            @blur="formData.number.isValid()" 
+            :class="{'error' : !formData.valid}">
+            <p class="error-message" v-if="!formData.number.valid">{{formData.number.errorMessage}}</p>
+            </div>
+         </div>   
+         
+         <button class="primary-button">Adicionar</button>
+         <button class="secondary-button" @click="hideAdressModal">Cancelar</button>
+
+        </div>
+     </Modal>
  </div>
 </template>
 
 <script>
+import Modal from '@/components/Modal';
+
 export default {
+    components: {
+        Modal 
+    },
     data() {
         return {
             formData: {
@@ -74,9 +130,54 @@ export default {
                     isValid: () => {
                         this.formData.cellphone.valid = this.formData.cellphone.value.length === 16;
                     }
-                }
-            }
-        }
+                },
+
+                cep: {
+                    value: '',
+                    placeholder: 'Digite o seu cep',
+                    errorMessage: 'O cep é obrigatório',
+                    label: 'CEP*', 
+                    valid: true,
+                    isValid: () => {
+                        this.formData.cep.valid = this.formData.cep.value.length;
+                    }
+                },
+
+                city: {
+                    value: '',
+                    placeholder: 'Digite o sua cidade',
+                    errorMessage: 'A cidade é obrigatória',
+                    label: 'Cidade*', 
+                    valid: true,
+                    isValid: () => {
+                        this.formData.city.valid = this.formData.city.value.length;
+                    }
+                },
+
+                street: {
+                    value: '',
+                    placeholder: 'Digite sua rua',
+                    errorMessage: 'A rua é obrigatória',
+                    label: 'Rua*', 
+                    valid: true,
+                    isValid: () => {
+                        this.formData.street.valid = this.formData.street.value.length;
+                    }
+                }, 
+
+                 number: {
+                    value: '',
+                    placeholder: 'Digite o número',
+                    errorMessage: 'O número é obrigatório',
+                    label: 'Número*', 
+                    valid: true,
+                    isValid: () => {
+                        this.formData.number.valid = this.formData.number.value.length;
+                    }
+                },
+            }, 
+            showAdressModal: false
+        };
     },
     methods: {
         triggerValidations() {
@@ -85,6 +186,12 @@ export default {
         },
         orderItens() {
             this.triggerValidations();
+        }, 
+        onShowAdressModal() {
+            this.showAdressModal = true;
+        },
+        hideAdressModal() {
+            this.showAdressModal = false; 
         }
     }
 }
@@ -98,18 +205,43 @@ export default {
     border-radius: 8px;
     padding: 30px 50px;
 
-    form {
-        display: flex;
-        flex-direction: column;
-
-        .input-field {
+    .input-field {
             display: flex;
             flex-direction: column;
+
+            label {
+            color: @write;
+            font-weight: 500;
+            font-size: 16px;
+            margin-bottom: 5px;
+        }
 
             & + .input-field {
                 margin-top: 15px;
             }
         }
+
+        .adress-container {
+            display: flex;
+            margin-top: 15px;
+
+            .input-field {
+                margin: 0;
+                width: 100%;
+
+                & + .input-field {
+                    width: 30%;
+                    margin-left: 15px;
+                }
+            }
+
+
+        }
+
+    form {
+        display: flex;
+        flex-direction: column;
+
 
         .section-title {
             color: @write;
@@ -118,12 +250,7 @@ export default {
             margin-bottom: 20px;
         }
 
-        label {
-            color: @write;
-            font-weight: 500;
-            font-size: 16px;
-            margin-bottom: 5px;
-        }
+        
 
         .error-message{
             font-size: 12px;
@@ -166,6 +293,16 @@ export default {
 
     button {
         margin: 30px auto;
+    }
+
+    .modal-content {
+        button{
+            text-align: center;
+
+            & + button { 
+                margin-left: 15px;
+            }
+        }
     }
 }
 
