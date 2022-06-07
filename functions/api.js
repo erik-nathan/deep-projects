1d52e1d8492c1db94c0bae2bb38047801d463c1d
erik-nathan <eriknathan.contato@gmail.com>
2022-06-07 19:06:22 -0300

    Teste deploy in netlify

--- functions/api.js
+++ functions/api.js
@@ -1,32 +1,29 @@
-import data from "../db.json";
-// const data = require("../db.json");
+const data = require('../db.json');
 
-export function handler (event, context, callback) {
-  // let path = event.path.replace('/api', '');
-  let path = event.path.split("/api")[1];
-  path = path.replace("/", "");
-  // console.log('path--->', event.path)
+exports.handler = function(event, context, callback) {
+    // let path = event.path.replace('/api', '');
+    let path = event.path.split('/api')[1];
+    path = path.replace('/', '');
+    // console.log('path--->', event.path)
+    
+    let id = null;
 
-  let id = null;
+    if(path.includes('/')) {
+        id = path.split('/')[1];
+        path = path.split('/')[0];
+    }
+    console.log('mypath-->', path)
+    // console.log('event-->', event)
+    console.log('id--->', id)
 
-  if (path.includes("/")) {
-    id = path.split("/")[1];
-    path = path.split("/")[0];
-  }
-  console.log("mypath-->", path);
-  // console.log('event-->', event)
-  console.log("id--->", id);
-
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      /* Required for CORS support to work */
-      "Access-Control-Allow-Origin": "*",
-      /* Required for cookies, authorization headers with HTTPS */
-      "Access-Control-Allow-Credentials": true,
-    },
-    body: id
-      ? JSON.stringify(data[path].find((item) => item.id === id))
-      : JSON.stringify(data[path]),
-  });
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            /* Required for CORS support to work */
+            'Access-Control-Allow-Origin': '*',
+            /* Required for cookies, authorization headers with HTTPS */
+            'Access-Control-Allow-Credentials': true
+        },
+        body: id ? JSON.stringify(data[path].find(item => item.id === id)) : JSON.stringify(data[path])
+    })
 }
